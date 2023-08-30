516e0745c05f289d443017378fbee8af5fd10594
HyeonsuLee <leehyeonsu4888@naver.com>
2023-08-31 01:18:53 +0900

    test-first-commit

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -3,7 +3,7 @@ import style from './MainPage.module.scss';
 import tittleImg from '../../assets/images/interfaces/tittle.png';
 
 export default function MainPage() : JSX.Element{
-
+    // 브랜치 테스트
     return (
         <div className={style.container}>
             <img className={style.tittle} src={tittleImg} alt="Title"></img>
